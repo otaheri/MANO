e62d434c5d7ff7b9927c1ed4c3f582c9931d419e
Omid Taheri <omid.taheri@tuebingen.mpg.de>
2020-08-04 15:05:52 +0200

    Major commit via upload

---
+++ joints_info.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
+# holder of all proprietary rights on this computer program.
+# You can only use this computer program if you have closed
+# a license agreement with MPG or you get the right to use the computer
+# program from someone who is authorized to grant you that right.
+# Any use of the computer program without a valid license is prohibited and
+# liable to prosecution.
+#
+# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
+# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
+# for Intelligent Systems. All rights reserved.
+#
+# Contact: ps-license@tuebingen.mpg.de
+
+TIP_IDS = {
+    'mano': {
+            'thumb':		744,
+            'index':		320,
+            'middle':		443,
+            'ring':		    554,
+            'pinky':		671,
+        }
+}
+
+JOINT_NAMES = [
+    'wrist',
+    'index1',
+    'index2',
+    'index3',
+    'middle1',
+    'middle2',
+    'middle3',
+    'pinky1',
+    'pinky2',
+    'pinky3',
+    'ring1',
+    'ring2',
+    'ring3',
+    'thumb1',
+    'thumb2',
+    'thumb3',    
+    'thumb_tip',
+    'index_tip',
+    'middle_tip',
+    'ring_tip',
+    'pinky_tip',
+]
