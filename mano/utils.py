967ad130cc74689587a63ad8d7d22942e74ac058
Omid Taheri <omid.taheri.71@gmail.com>
2020-08-12 10:23:07 +0200

    resolved issue with to_tensor

--- mano/utils.py
+++ mano/utils.py
@@ -22,21 +22,24 @@ import numpy as np
 import torch
 
 
-def to_tensor(array, dtype=torch.float32):
-    if 'torch.tensor' not in str(type(array)):
-        return torch.tensor(array, dtype=dtype)
-
-
 class Struct(object):
     def __init__(self, **kwargs):
         for key, val in kwargs.items():
             setattr(self, key, val)
 
 
+def to_tensor(array, dtype=torch.float32):
+    if not torch.is_tensor(array):
+        array = torch.tensor(array)
+    return array.to(dtype)
+
+
 def to_np(array, dtype=np.float32):
     if 'scipy.sparse' in str(type(array)):
-        array = array.todense()
-    return np.array(array, dtype=dtype)
+        array = np.array(array.todencse(), dtype=dtype)
+    elif torch.is_tensor(array):
+        array = array.detach().cpu().numpy()
+    return array
 
 
 def rot_mat_to_euler(rot_mats):
